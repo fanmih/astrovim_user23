be90a4538a7fab38a49b8b8189f0f14bd8ef5655
Michael Fanariotis <fanmih@gmail.com>
2023-08-13 16:36:05 +0200

    Added colorschemes and language packs

--- plugins/community.lua
+++ plugins/community.lua
@@ -4,6 +4,13 @@ return {
   -- example of importing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
-  -- { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.colorscheme.rose-pine" },
+  { import = "astrocommunity.pack.ansible" },
+  { import = "astrocommunity.pack.python" },
+  { import = "astrocommunity.pack.bash" },
+  { import = "astrocommunity.pack.docker" },
+  { import = "astrocommunity.pack.markdown" },
+  { import = "astrocommunity.pack.toml" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
 }
